54d58e5f5aad8921c547fbe4f2edf5c211a43a68
Manuel Sanchez del Rio <srio@esrf.eu>
2018-10-27 15:06:59 +0200

    updated doc

--- setup.py
+++ setup.py
@@ -4,8 +4,12 @@
 #
 # Install from sources: 
 #     git clone https://github.com/srio/xray-wave-propagators
-#     cd minishadow
-#     /Applications/Oasys1.1.app/Contents/MacOS/PythonApp -m pip install -e . --no-deps --no-binary :all:
+#     cd xray-wave-propagators
+#     python -m pip install -e . --no-deps --no-binary :all:
+#
+#
+# Use (in python):
+# from sajidpropagators.prop import *
 #
 # Upload to pypi (when uploading, increment the version number):
 #     python setup.py register (only once, not longer needed)
@@ -17,7 +21,7 @@ from setuptools import setup
 
 setup(name='sajidpropagators',
       version='0.1',
-      description='',
+      description='sajid propagators',
       author='Sajid Ali',
       author_email='sajidsyed2021@u.northwestern.edu',
       url='https://github.com/s-sajid-ali/xray-wave-propagators',
